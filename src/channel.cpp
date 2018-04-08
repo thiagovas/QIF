f67d000bcd038ad18bc16e69699ab42514ecbba6
Artur Vaz <vaz985@gmail.com>
2018-04-08 20:36:59 -0300

    Compatible channels and old metrics

--- src/channel.cpp
+++ src/channel.cpp
@@ -1,6 +1,4 @@
 #include "channel.h"
-#include <random>
-#include <iostream>
 int main() {
   Channel c(2,3);
   std::cout << c << std::endl;
@@ -21,26 +19,23 @@ void Channel::ParseInput(std::string input_str) {}
 // The input becomes the output, and the output becomes the
 // input; matrix-wise we'll have p(x|y) instead of p(y|x).
 void Channel::Transpose() {
-//  if( this->h_matrix.capacity == 0  ) {
-//    std::vector<double> p_y;
-//    this->h_matrix = this->c_matrix;
-//    for( int i=0; i<this->n_in_; i++ ) {
-//      for( int j=0; j<this->n_out_; j++ ) {
-//        this->h_matrix[i][j] = this->h_matrix[i][j] * this->prior_distribution[i];
-//      }
-//    }
-//
-//    for( int i=0; i<this->n_out_; i++ ) {
-//      p_y.push_back(0.0);
-//      for( int j=0; j<this->n_in_; j++ ) {
-//        p_y[i] += this->h_matrix[j][i]; 
-//      }
-//      for( int j=0; j<this->n_in_; j++ ) {
-//        this->h_matrix[j][i] /= p_y[i];
-//      }
-//    } 
-//    
-//  }
+  std::vector<double> p_y;
+  this->h_matrix = this->c_matrix;
+  for( int i=0; i<this->n_in_; i++ ) {
+    for( int j=0; j<this->n_out_; j++ ) {
+      this->h_matrix[i][j] = this->h_matrix[i][j] * this->prior_distribution[i];
+    }
+  }
+
+  for( int i=0; i<this->n_out_; i++ ) {
+    p_y.push_back(0.0);
+    for( int j=0; j<this->n_in_; j++ ) {
+      p_y[i] += this->h_matrix[j][i]; 
+    }
+    for( int j=0; j<this->n_in_; j++ ) {
+      this->h_matrix[j][i] /= p_y[i];
+    }
+  } 
 }
 
 // This function randomizes the current channel.
@@ -68,6 +63,12 @@ void Channel::Randomize() {
   }  
 }
 
+bool Channel::CompatibleChannels(Channel c1, Channel c2) {
+  if(c1.n_in_ == c2.n_in_ && c1.in_names_ == c2.in_names_)
+    return true;
+  return false;
+}
+
 std::ostream& operator<< (std::ostream& stream, const Channel& c) {
   for( int i=0; i<c.n_in_; i++ ) {
     for( int j=0; j<c.n_out_; j++ ) {
@@ -77,4 +78,48 @@ std::ostream& operator<< (std::ostream& stream, const Channel& c) {
       stream << std::endl;
   }
   return stream;
+}
+double Channel::ShannonEntropy() {
+  double entropy = 0;
+  for(int i=0; i<this->n_out_; i++){
+    entropy += (this->prior_distribution[i]*log2(1/this->prior_distribution[i]));
+  } 
+  return entropy;
+}
+double Channel::ConditionalEntropy() {
+  double entropy = 0;
+  for(int j=0; j<this->n_out_; j++){
+    double conditional_entropy_X = 0;
+    for(int i=0; i<this->n_in_; i++){
+      conditional_entropy_X += (this->h_matrix[i][j] * log2(1/this->h_matrix[i][j]));
+    }
+    entropy += (this->out_distribution[j] * conditional_entropy_X);
+  }
+  return entropy;
+}
+double Channel::JointEntropy() {
+  double entropy = 0;
+  std::vector<std::vector<double> > v = this->c_matrix;
+  for(int i=0; i<this->n_in_; i++) {
+    for(int j=0; j<this->n_out_; j++) {
+      v[i][j] = v[i][j] * this->prior_distribution[i]; 
+    }
+  }
+  for(int i=0; i<this->n_in_; i++) {
+    for(int j=0; j<this->n_out_; j++) {
+      entropy += (v[i][j]*log2(1/v[i][j]));
+    }
+  } 
+  return entropy;
+}
+double Channel::GuessingEntropy() {
+  double entropy = 0;
+  std::vector<double> v = this->prior_distribution;
+  sort(v.begin(),v.end());
+  for(int i=1; i<=this->n_in_; i++)
+    entropy += (i*v[i]);
+  return entropy;
+}
+double Channel::MutualInformation() {
+  return (this->ConditionalEntropy() - this->ShannonEntropy());
 }
--- src/channel.h
+++ src/channel.h
@@ -3,6 +3,14 @@
 #define channel_h
 #include <string>
 #include <vector>
+#include <random>
+#include <iostream>
+#include <cmath>
+
+// Channel matrix
+// x_0 -> y_0, y_1, ..., y_m
+// ...
+// x_n -> y_0, y_1, ..., y_m
 
 class Channel {
   public:
@@ -14,28 +22,24 @@ class Channel {
     // This function returns a string that represents the
     // current channel.
     std::string to_string();
-    
-    // This function transposes the current channel.
-    // The input becomes the output, and the output becomes the
-    // input; matrix-wise we'll have p(x|y) instead of p(y|x).
-    void Transpose();
-    
-    // This function randomizes the current channel.
-    // Maintaining the channel dimensions.
-    void Randomize();
+
+    bool CompatibleChannels(Channel c1, Channel c2);
     
     friend std::ostream& operator<< (std::ostream& stream, const Channel& c);
 
-  private:
 
+  private:
     // This is the channel matrix. ( p(y|x) )
     std::vector<std::vector<double> > c_matrix;
-
     // This is the posterior probability matrix. ( hyper distribution p(x|y) )
     std::vector<std::vector<double> > h_matrix;
-
     // This is the prior distribution.
     std::vector<double> prior_distribution;
+    // This is the output distribution.
+    std::vector<double> out_distribution;
+
+    // This is the joint matrix. ( p(x,y) )
+    std::vector<std::vector<double> > j_matrix;
 
     // These ints keep the number of input lines we have,
     // and the number of output lines.
@@ -44,6 +48,23 @@ class Channel {
     // These vectors keep the names of each input line and
     // each output line
     std::vector<std::string> in_names_, out_names_;
+
+    // This function randomizes the current channel.
+    // Maintaining the channel dimensions.
+    void Randomize();
+
+    // This function transposes the current channel.
+    // The input becomes the output, and the output becomes the
+    // input; matrix-wise we'll have p(x|y) instead of p(y|x).
+    void Transpose();
+
+    // OLD METRICS
+    // In bits
+    double ShannonEntropy();
+    double ConditionalEntropy();
+    double JointEntropy();
+    double GuessingEntropy();
+    double MutualInformation();
 };
 
 #endif
