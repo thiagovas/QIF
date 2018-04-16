de5e7c13b3cb9af666cad3387ef86e0771628166
Thiago Silva <thiagovieiraas@gmail.com>
2018-04-15 21:10:22 -0300

    Updated the random brute-force; now it prints the best pair of channels for each dimension settings.

--- channel/binary_sources/random_brutao.cpp
+++ channel/binary_sources/random_brutao.cpp
@@ -28,21 +28,47 @@ int main()
   for(int i = 2; i < 10; i++) {
     for(int j = 2; j < 10; j++) {
       Channel c1(i, j), c2(i, j);
-
-      int ntries = 10000;
-      while(ntries--) {
+      Channel best_c1, best_c2;
+      double best_diffa = 0, best_diffb = 0;
+      
+      int ntries = 100000;
+      while((ntries--) > 0 || best_diffa < EPS || best_diffb < EPS) {
         while(fabs(c1.NormalizedMutualInformation() -
                     c2.NormalizedMutualInformation()) > EPS) {
           c1.Randomize();
           c2.Randomize();
         }
-
-        if(b.LeakageMultPosterior(c1) > b.LeakageMultPosterior(c2) &&
-            b.LeakageMultReversePosterior(c1) < b.LeakageMultReversePosterior(c2)) {
-          cout << "OIA\n";
-          cout << c1.to_string() << endl << endl;
-          cout << c2.to_string() << endl;
+        
+        double bLeakagePosterior_c1 = b.LeakageMultPosterior(c1);
+        double bLeakagePosterior_c2 = b.LeakageMultPosterior(c2);
+        
+        double bLeakageRevPosterior_c1 = b.LeakageMultReversePosterior(c1);
+        double bLeakageRevPosterior_c2 = b.LeakageMultReversePosterior(c2);
+        
+        double diff1 = bLeakagePosterior_c1 - bLeakagePosterior_c2;
+        double diff2 = bLeakageRevPosterior_c2 - bLeakageRevPosterior_c1;
+        
+        if(bLeakagePosterior_c1 > bLeakagePosterior_c2 && 
+            bLeakageRevPosterior_c1 < bLeakageRevPosterior_c2) {
+          if(diff1 > best_diffa && diff2 > best_diffb) {
+            best_c1.ParseInput(c1.to_string());
+            best_c2.ParseInput(c2.to_string());
+            best_diffa = diff1;
+            best_diffb = diff2;
+          }
         }
+        c1.Randomize();
+        c2.Randomize();
+      }
+
+      if(best_diffa > 0 && best_diffb > 0) {
+        cout << "OIA: " << i << " X " << j << endl;
+        cout << best_c1.to_string() << endl;
+        cout << best_c2.to_string() << endl;
+        cout << b.LeakageMultPosterior(best_c1) << " " << b.LeakageMultPosterior(best_c2) << endl;
+        cout << b.LeakageMultReversePosterior(best_c1) << " " << b.LeakageMultReversePosterior(best_c2) << endl;
+
+        cout << endl << endl;
       }
     }
   }
