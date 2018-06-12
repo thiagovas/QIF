56a30a91b8e25d8d193eebeecf82b32376d5cfdf
Thiago Silva <thiagovieiraas@gmail.com>
2018-06-12 08:48:39 -0300

    Add googletest submodule, and some config files for bazel

--- base/binary_sources/distribution_tst.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "../../channel/channel.h"
-#include "../distribution.h"
-int main() {
-  base::Distribution d1(5); 
-  std::cout << d1.GuessingEntropy() << std::endl;
-  return 0;
-}
