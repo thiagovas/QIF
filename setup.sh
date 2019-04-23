a68703f1519ab608cc890db7e3f1e0b8b718e694
Thiago Silva <thiagovieiraas@gmail.com>
2019-04-23 16:48:22 -0300

    Updated the setup script with the newest version of bazel.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@
 sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python
 export PATH="$PATH:$HOME/bin"
 
-wget https://github.com/bazelbuild/bazel/releases/download/0.14.1/bazel-0.14.1-installer-linux-x86_64.sh
-chmod +x bazel-0.14.1-installer-linux-x86_64.sh
-./bazel-0.14.1-installer-linux-x86_64.sh --user
-rm bazel-0.14.1-installer-linux-x86_64.sh
+wget https://github.com/bazelbuild/bazel/releases/download/0.24.1/bazel-0.24.1-installer-linux-x86_64.sh
+chmod +x bazel-0.24.1-installer-linux-x86_64.sh
+./bazel-0.24.1-installer-linux-x86_64.sh --user
+rm bazel-0.24.1-installer-linux-x86_64.sh
 
 echo 'source ~/.bazel/bin/bazel-complete.bash' >> ~/.bashrc
